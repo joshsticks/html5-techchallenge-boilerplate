c81e7346474f68ec60e065a1a4797b20de270968
Josh Stickles <sticklesjb@gmail.com>
2014-07-08 18:33:01 -0400

    added some better boiler plate stuff

--- js/app.js
+++ js/app.js
@@ -5,8 +5,11 @@
 	});
 
 	// A list of articles that are featured on the main page
-	var MainArticles = Backbone.Collection.extend({
+	var MainArticles = Backbone.Model.extend({
+		url: "http://html5news.herokuapp.com/articles/featured",
+		initialize: function (options) {
 
+		}
 	});
 
 	// A list of articles in a single category
@@ -16,12 +19,31 @@
 
 	// A view used to render out lists of articles
 	var PageView = Backbone.View.extend({
-
+		el: $("#content"),
+          		template: _.template($("#page-template").html()),
+          		initialize: function (options) {
+          			var that = this;
+          			this.model.fetch({
+          				success: function( results ) {
+          					that.render();
+          				}
+          			});
+          		},
+          		render: function () {
+            		this.$el.html(this.template(this.model));
+               		return this;
+          		}
 	});
 
 	// A router that handles our single page app's navigation
-	var AppRouter = Backbone.Router.extend({
+	var AppRouter = Backbone.Router.extend({	
+		routes: {
+               		"": "home"
+          		},
 
+		home: function () {
+			var pageView = new PageView({model: new MainArticles()});
+          		}
 	});
 
 	var appRouter = new AppRouter();
