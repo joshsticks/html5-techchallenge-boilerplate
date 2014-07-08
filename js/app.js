6193aeec6baa2d633dc2d80fafdaa5f6a3bf508f
Josh Stickles <sticklesjb@gmail.com>
2014-07-08 10:14:05 -0400

    added some more boilerplate

---
+++ js/app.js
@@ -0,0 +1,28 @@
+(function ($) {
+	// A generic article in our app
+	var Article = Backbone.Model.extend({
+
+	});
+
+	// A list of articles that are featured on the main page
+	var MainArticles = Backbone.Collection.extend({
+
+	});
+
+	// A list of articles in a single category
+	var CategoryArticles = Backbone.Collection.extend({
+
+	});
+
+	// A view used to render out lists of articles
+	var PageView = Backbone.View.extend({
+
+	});
+
+	// A router that handles our single page app's navigation
+	var AppRouter = Backbone.Router.extend({
+
+	});
+
+	Backbone.history.start();
+}) (jQuery);
